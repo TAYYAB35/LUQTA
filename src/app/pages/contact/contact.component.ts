206d0bdaa24367c07667e07f43c532da9bf8813e
TAYYAB35 <tayyabfmtech@gmail.com>
2025-01-15 12:23:23 +0500

    footer add

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
 import { FaqComponent } from '../../components/faq/faq.component';
+import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [HeaderComponent,FaqComponent],
+  imports: [HeaderComponent,FaqComponent, FooterComponent],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
