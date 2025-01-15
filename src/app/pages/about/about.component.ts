e594cafda3a6a2ca30d344f32482a55408bed702
TAYYAB35 <tayyabfmtech@gmail.com>
2025-01-15 10:35:24 +0500

    error fixing

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
+import { FaqComponent } from '../../components/faq/faq.component';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [CommonModule, HeaderComponent],
+  imports: [CommonModule, HeaderComponent, FaqComponent],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
